458b3c57f88bcd565630c8be51b9832a28985fe3
chenq86 <87509477+chenq86@users.noreply.github.com>
2021-07-16 03:16:08 -0400

    Update pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(() => {
+    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    led.plotBarGraph(p, 0);
+})
