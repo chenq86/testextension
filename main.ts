37864f0ff91a79e4a877da0f97ae19d800aac5d8
chenq86 <87509477+chenq86@users.noreply.github.com>
2021-07-16 22:30:24 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ namespace ICBit {
      * @param unit desired conversion unit
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
-    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit" weight=29
     export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
